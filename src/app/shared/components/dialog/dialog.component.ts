e517db841f9f2fb68d5872baf01fb563c35a288d
RaCode75 <racode75@gmail.com>
2022-11-06 14:29:37 -0300

    dialog updated

--- src/app/shared/components/dialog/dialog.component.ts
+++ src/app/shared/components/dialog/dialog.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { SharedModule } from '@shared/shared.module'; 
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ab-dialog',
@@ -7,13 +8,19 @@ import { SharedModule } from '@shared/shared.module';
   styleUrls: ['./dialog.component.scss']
 })
 export class DialogComponent implements OnInit {
-titulo: string = "Esto es un Dialog"
-longText: string = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- Donec rutrum sit amet eros ac scelerisque. Duis dolor tellus, commodo nec orci nec,
-  tristique mattis purus. Proin tempus, turpis eu dignissim lacinia, est massa dictum
-  tortor, et tristique sem diam nec diam. Donec et est felis. Cras ornare suscipit
-  nisl, quis elementum est.`
-  constructor() { }
+
+resultado: boolean = false;
+// titulo: string = "Esto es un Dialog"
+// longText: string = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+//  Donec rutrum sit amet eros ac scelerisque. Duis dolor tellus, commodo nec orci nec,
+//   tristique mattis purus. Proin tempus, turpis eu dignissim lacinia, est massa dictum
+//   tortor, et tristique sem diam nec diam. Donec et est felis. Cras ornare suscipit
+//   nisl, quis elementum est.`
+
+    constructor(public dialogRef:MatDialogRef<DialogComponent>,
+      @Inject(MAT_DIALOG_DATA) public data: any,
+      private router:Router) {}
+  
 
   ngOnInit(): void {
   }
